0867b92c81fb9d48ac97b8e0c0ab7f27ee2cb857
Mitchap <mitchbarcenilla4@gmail.com>
2024-09-24 21:36:36 +0800

    te amo

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <main class="relative overflow-hidden">
+    <div class="flex items-center justify-center h-screen flex-col gap-10">
+
+      <img :src="catImage" alt="Cat" class="max-w-40 max-h-40 md:max-w-64 md:max-h-64">
+      <h1 v-if="!buttonClicked" class="text-lg font-medium md:text-2xl">Will you still be my girlfriend, <a
+          href="https://www.facebook.com/danielyn.pantonial.5" class="font-bold hover:text-pink-600">Danielyn</a>?</h1>
+      <h1 v-else class="text-lg font-medium md:text-2xl ">Yipeeeee
+        <br><br>
+        <span class="fa-regular"> I <i class="fa-regular fa-solid fa-heart fa-bounce"></i> YOU</span>
+      </h1>
+
+
+      <div v-if="!buttonClicked" class="flex flex-col md:flex-row items-center gap-10">
+
+        <button id="approve_btn" @click="approve" type="button"
+          class="transition duration-400 p-2 md:p-4 bg-pink-400 hover:bg-pink-600 hover:text-neutral-100 rounded-lg">
+          Absolutely!
+        </button>
+        <button @mouseenter="moveNoButton" type="button" :style="noButtonStyle"
+          class="transition duration-400 p-2 md:p-4 bg-gray-400 hover:bg-red-600 hover:text-neutral-100 rounded-lg absolute">
+          No!
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      catImage: '/images/cat_heart1.gif',
+      buttonClicked: false,
+      noButtonStyle: {
+        left: '50%',
+        top: '10%',
+      }
+    };
+  },
+  methods: {
+    approve() {
+      this.catImage = '/images/cat_heart2.gif';
+      this.buttonClicked = true;
+    },
+    moveNoButton() {
+      const randomX = Math.floor(Math.random() * (window.innerWidth - 200)); // Random X within the viewport
+      const randomY = Math.floor(Math.random() * (window.innerHeight - 200)); // Random Y within the viewport
+      this.noButtonStyle = {
+        left: `${randomX}px`,
+        top: `${randomY}px`,
+        position: 'absolute',
+
+      };
+    }
+  },
+};
+
+
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,41 @@
+<!-- <template>
+    <main>
+        <div class="flex items-center justify-center h-screen flex-col gap-10">
+            <!-- bind img src -->
+<img id="cat_image" :src="imageSource" alt="" class="w-40 h-40 md:w-64 md:h-64">
+<h1 class="text-lg font-medium md:text-2xl">Will you still be my girlfriend, <span class="font-bold">Danielyn</span>?
+</h1>
+<div class="flex flex-col md:flex-row items-center gap-5">
+    <button @click="yes_button" type="button"
+        class="transition duration-400 p-4 bg-pink-400 hover:bg-pink-600 hover:text-neutral-100 rounded-lg ">
+        Absolutely!
+    </button>
+    <button type="button"
+        class="transition duration-400 p-4 bg-gray-400 hover:bg-red-600 hover:text-neutral-100 rounded-lg ">
+        Nooo!
+    </button>
+</div>
+</div>
+</main>
+
+</template>
+
+<script>
+import { createApp, ref } from 'vue'
+
+createApp({
+    setup() {
+        return {
+            imageSource: 'images/cat_heart1.gif', // Initial image
+            image1: 'images/cat_heart2.gif',      // First image source
+            // image2: 'https://via.placeholder.com/300x200?text=Image+2',      // Second image source
+        };
+    },
+    methods: {
+        // Method to switch between images
+        yes_button() {
+            this.imageSource = this.imageSource === this.image1;
+        }
+    }
+}).mount('#app')
+</script> -->
